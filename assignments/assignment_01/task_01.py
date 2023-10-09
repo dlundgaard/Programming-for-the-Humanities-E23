ebfa4456257aadb2fde8f42d1f56131d1a67b52b
Daniel Lundgaard <dlundgaard8@gmail.com>
2023-10-09 16:08:13 +0200

    minor changes

--- assignments/assignment_01/task_01.py
+++ assignments/assignment_01/task_01.py
@@ -2,20 +2,31 @@ import sys
 import time
 
 """
-Indefinitely outputs left-aligned alternating wave to terminal until aborted.
-Amplitude and period of wave adjustable through function arguments.
+Indefinitely outputs left-aligned alternating wave to terminal.
+Amplitude (and thus period width/duration) of wave adjustable through function arguments.
+Number of periods run before terminating can be specified, otherwise indefinite or until keyboard interrupt.
 """
-def alternating_wave(peak_width = 9, period = 1):
+def alternating_wave(peak_width = 9, cycles = None):
     assert peak_width > 1
+    assert cycles > 0
+
+    current_width = 0
+    rising = True
+    cycle_index = 0
+
     try:
-        current_width = 0
-        rising = True
         while True:
             if current_width == peak_width:
                 rising = False
             elif current_width == 0:
                 rising = True
-            current_width = current_width + 1 if rising else current_width - 1 # increment if currently rising, decrement otherwise
+                cycle_index += 1
+
+            current_width = current_width + 1 if rising else current_width - 1
+
+            if cycles and cycle_index > cycles:
+                break
+
             if current_width:
                 print("*" * current_width)
                 time.sleep(1 / (peak_width * 2)) # scale to respect Hz unit for period
@@ -23,4 +34,4 @@ def alternating_wave(peak_width = 9, period = 1):
         sys.exit(0)
 
 if __name__ == "__main__":
-    alternating_wave()
+    alternating_wave(cycles = 2)
